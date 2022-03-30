355d65b3f68f50767860655e895bd9904681a172
tiago luz <tiagoluzgpr@gmail.com>
2022-03-30 19:12:24 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { CheckInComponent } from './check-in/check-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '**', redirectTo: 'check-in' },
+  { path: 'check-in', component: CheckInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/check-in/checkIn.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckInComponent } from './check-in.component';
+
+
+
+@NgModule({
+  declarations: [
+    CheckInComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CheckInModule { }
