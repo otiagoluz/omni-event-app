0619d8f26fa21669dc4b383ac633c47e50b7b140
tiago luz <tiagoluzgpr@gmail.com>
2022-04-04 01:02:41 -0400

    fix

--- src/app/server.ts
+++ src/app/server.ts
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function(req: any, res: any) {
 
 res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
